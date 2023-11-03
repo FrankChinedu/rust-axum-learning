d470544f1596f18da607e35c025ce97c3785452e
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-11-03 08:41:14 +0100

    basic sets up for routing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
+mod error;
+
 use axum::{
-    extract::Query,
+    extract::{Path, Query},
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, get_service},
     Router,
 };
 use serde::Deserialize;
 use std::net::SocketAddr;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_all = Router::new()
+        .merge(route_hello())
+        .fallback_service(route_static());
 
     // region: -
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap()
+}
+
+fn route_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+fn route_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
 }
 
 #[derive(Debug, Deserialize)]
@@ -29,6 +44,12 @@ async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello {params:?}", "HANDLER");
 
     let name = params.name.as_deref().unwrap_or("World---");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 {name:?}", "HANDLER");
 
     Html(format!("Hello <strong>{name}</strong>"))
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -4,7 +4,7 @@ use anyhow::{Ok, Result};
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/hello?name=Angelo").await?.print().await?;
+    hc.do_get("/hello2/frank").await?.print().await?;
 
     Ok(())
 }
