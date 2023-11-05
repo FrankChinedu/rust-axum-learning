c2c5e3e429a376e403877d221397e15d8ba3a448
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-11-05 23:34:59 +0100

    client and server error resolvers

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod error;
 mod model;
 mod web;
 
+use axum::Json;
 use axum::{
     extract::{Path, Query},
     middleware,
@@ -14,9 +15,11 @@ use axum::{
     Router,
 };
 use serde::Deserialize;
+use serde_json::json;
 use std::net::SocketAddr;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -50,8 +53,30 @@ async fn main() -> Result<()> {
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string()
+                }
+            });
+
+            println!("   ->> client_error_body: {}", client_error_body);
+            (*status_code, Json(client_error_body)).into_response()
+        });
+    println!(
+        "   ->> server log line - {} - Error: {:?}",
+        uuid, service_error
+    );
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 
 fn route_static() -> Router {
